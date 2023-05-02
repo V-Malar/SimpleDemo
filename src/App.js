5f6fc9d53fdbf0ade7faa7867333bcb0f0a9bee1
V-Malar <128116398+V-Malar@users.noreply.github.com>
2023-05-02 20:10:05 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import Counter from './pages/Counter';
+import Calculator from './pages/Calculator';
+import Login from './components/Forms/Login';
+import Todo from './components/Forms/Todo';
+import Nopage from './pages/Nopage';
+import Fetch from './components/Forms/Fetch';
+
+
+
+function App() {
+  return (
+    <React.Fragment>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout/>}>
+            <Route index element={<Home/>}/>
+            <Route path="counter" element={<Counter/>}/>
+            <Route path="calculator" element={<Calculator/>}/>
+            <Route path="login" element={<Login/>}/>
+            <Route path="todo" element={<Todo/>}/>
+            <Route path="fetch" element={<Fetch/>}/>
+            <Route path = "*" element = {<Nopage/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </React.Fragment>
+  );
+}
+
+export default App;
